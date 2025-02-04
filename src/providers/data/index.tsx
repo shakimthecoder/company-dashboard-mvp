0ea51b19524c14b9eb7240eb7c86ecf73314280c
Shakim Harris <amenraprod@gmail.com>
2025-02-04 17:29:56 -0500

    completed ws

--- src/providers/data/index.tsx
+++ src/providers/data/index.tsx
@@ -1,4 +1,6 @@
-import { GraphQLClient } from "@refinedev/nestjs-query";
+import graphqlDataProvider, 
+{ GraphQLClient, 
+liveProvider as graphqlLiveProvider } from "@refinedev/nestjs-query";
 import { fetchWrapper } from "./fetch-wrapper";
 import { createClient } from "graphql-ws";
 
@@ -31,3 +33,6 @@ export const wsClient = typeof window !== "undefined"
     }
 })
 : undefined;
+
+export const dataProvider = graphqlDataProvider(client);
+export const liveProvider = wsClient ? graphqlLiveProvider(wsClient) : undefined;
